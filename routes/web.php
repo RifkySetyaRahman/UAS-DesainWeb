e6a82ba9e4da647dafa917e1887d2451e67c1ce8
Budyyy <rielopambudi73@gmail.com>
2024-12-14 18:43:06 +0800

    Tes ke 2

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,19 @@ use App\Http\Controllers\DashboardController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('user.login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/register', [AuthController::class, 'showRegisteration'])->name('registrasi');
+Route::post('/register', [AuthController::class, 'registrasi']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('user.logout');
+
+Route::resource('karyawans', KaryawanController::class);
+
+Route::resource('departemens', DepartemenController::class);
+
+Route::resource('posisis', PosisiController::class);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
